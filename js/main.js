c1a251d89c155ae9b23c52f87058f21e85ee41e7
Jeremy Lecour <jeremy.lecour@gmail.com>
2010-04-07 00:48:06 +0200

    Affichage des 3 derniers tweets en page d'accueil (pas encore de CSS)

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ $('a[href][rel="external"]').live('click', function() {
   window.open(this.href,'','');
   return false;
 });
+
+jQuery(document).ready(function($) {
+  $("#tweets").tweet({
+    username: "plugfr",
+    count: 3,
+    loading_text: "chargement ..."
+  });
+});
