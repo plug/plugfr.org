399db4c3f16af771b9e349c66d6048f5c5b92d19
Colin Darie <colindarie@gmail.com>
2010-04-22 20:36:04 +0200

    Fait gagner 2 points à Yslow en réduisant de 21% la taille de l'ensemble des fichiers textes en supprimant les lignes vides
    Utiliser un script après génération plutôt que modifier les fichiers sources permet de garder les fichiers sources lisibles. Pour l'instant, les fichiers concernés portent les extensions html, xml, css, js et le fichier .htaccess.

--- script/publish.sh
+++ script/publish.sh
@@ -5,15 +5,20 @@ rm -rf _site/*
 jekyll --no-auto
 
 if [ $? -eq 0 ]; then
+  echo "Remove empty lines from generated files"
+  ./script/remove_empty_lines.sh
+fi
+
+if [ $? -eq 0 ]; then
   SITEMAP=_site/sitemap.xml
-  
+
   if [ -f $SITEMAP ]; then
     if [ -f $SITEMAP.gz ]; then
       rm $SITEMAP.gz
     fi
     gzip -c $SITEMAP > $SITEMAP.gz
   fi
-  
+
   # rsync -aH --exclude=.git --delete-excluded _site/ lecour@lecour.fr:jeremy/plug/
   rsync -aH --exclude=.git --delete-excluded _site/ plug@ssh.tuxfamily.org:plugfr/plugfr.org-web/htdocs/
   if [ $? -eq 0 ]; then
@@ -23,3 +28,4 @@ else
   echo "----"
   echo "Impossible de re-générer le site Jekyll a retourné l'erreur $?"
 fi
+
---
+++ script/remove_empty_lines.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cd _site
+
+remove_empty_lines ()
+{
+  TMP=`mktemp`
+  grep -v -E "^\s*$" "$1" > $TMP
+
+  # do not use mv or cp to preserve permissions and owner!
+  cat $TMP > "$1"
+  rm $TMP
+}
+
+find . \( -name "*.html" -o -name "*.xml" -o -name "*.css" -o -name "*.js" -o -name ".htaccess" \) -print | while read F
+  do
+    remove_empty_lines "$F"
+  done
+
+exit 0
+
