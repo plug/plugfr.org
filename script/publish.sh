5ed9c291a0a4c4b517dad75c4e8e01b6a2bd168c
Colin Darie <colindarie@gmail.com>
2010-05-02 18:15:34 +0200

    Le script publish vérifie si on a un accès au serveur, donc peut êtreé exécuté sans problème pour ceux qui n'ont pas l'accès
    Bonus du dimanche : les messages d'erreur sont envoyés sur la sortie stderr

--- script/publish.sh
+++ script/publish.sh
@@ -19,13 +19,26 @@ if [ $? -eq 0 ]; then
     gzip -c $SITEMAP > $SITEMAP.gz
   fi
 
-  # rsync -aH --exclude=.git --delete-excluded _site/ lecour@lecour.fr:jeremy/plug/
-  rsync -aH --exclude=.git --delete-excluded _site/ plug@ssh.tuxfamily.org:plugfr/plugfr.org-web/htdocs/
-  if [ $? -eq 0 ]; then
-    echo "Site uploaded to 'TuxFamily' : http://plugfr.org/"
-  fi
-else
-  echo "----"
-  echo "Impossible de re-générer le site Jekyll a retourné l'erreur $?"
-fi
+ 
+  # Check if we have an access on remote server, so users without access can run this script without "real" error
+  REMOTE=plug@ssh.tuxfamily.org:plugfr/
+  ssh -q -o "BatchMode=yes" ${REMOTE} "echo 2>&1 >/dev/null"
 
+  if [ $? -eq 0 ] ; then
+    rsync -aH --exclude=.git --delete-excluded _site/ ${REMOTE}plugfr.org-web/htdocs/
+    #rsync -aH --exclude=.git --delete-excluded _site/ lecour@lecour.fr:jeremy/plug/
+    
+    if [ $? -eq 0 ]; then
+      echo "Site uploaded to 'TuxFamily' : http://plugfr.org/"
+    else
+      echo "Rsync a retourné l'erreur $? lors de l'upload" 1>&2
+    fi
+ 
+  else
+    echo "Vous n'avez pas d'accès sur le serveur distant, ou le serveur est injoignable" 1>&2
+  fi
+
+else
+ echo "----" 1>&2
+ echo "Impossible de re-générer le site Jekyll a retourné l'erreur $?" 1>&2
+fi
