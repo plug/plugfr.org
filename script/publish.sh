bf9c6224bee43e511258cf74fd84c84400c8747a
Colin Darie <colindarie@gmail.com>
2010-05-02 18:38:30 +0200

    Test de connexion ssh simplement avec l'host

--- script/publish.sh
+++ script/publish.sh
@@ -19,20 +19,20 @@ if [ $? -eq 0 ]; then
     gzip -c $SITEMAP > $SITEMAP.gz
   fi
 
- 
+
   # Check if we have an access on remote server, so users without access can run this script without "real" error
-  REMOTE=plug@ssh.tuxfamily.org:plugfr/
+  REMOTE=plug@ssh.tuxfamily.org
   ssh -q -o "BatchMode=yes" ${REMOTE} "echo 2>&1 >/dev/null"
 
   if [ $? -eq 0 ] ; then
-    rsync -aH --exclude=.git --delete-excluded _site/ ${REMOTE}plugfr.org-web/htdocs/
-    
+    rsync -aH --exclude=.git --delete-excluded _site/ ${REMOTE}:plugfr/plugfr.org-web/htdocs/
+
     if [ $? -eq 0 ]; then
       echo "Site uploaded to 'TuxFamily' : http://plugfr.org/"
     else
       echo "Rsync a retourné l'erreur $? lors de l'upload" 1>&2
     fi
- 
+
   else
     echo "Vous n'avez pas d'accès sur le serveur distant, ou le serveur est injoignable" 1>&2
   fi
@@ -41,3 +41,4 @@ else
  echo "----" 1>&2
  echo "Impossible de re-générer le site Jekyll a retourné l'erreur $?" 1>&2
 fi
+
