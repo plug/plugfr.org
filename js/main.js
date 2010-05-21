b26e10c238a27a125c0f7061e2b9e7fd1c91da02
Jeremy Lecour <jeremy.lecour@gmail.com>
2010-05-22 00:26:29 +0200

    Masquage des adresses des listes via un artifice Javascript

--- js/main.js
+++ js/main.js
@@ -9,4 +9,10 @@ jQuery(document).ready(function($) {
     count: 8,
     loading_text: "Chargement ..."
   });
+  $('.mel').each(function (i) {
+    var temp = $(this).html();
+    temp = temp.replace(/\/\//g,"@");
+    temp = temp.replace(/\*/g,".");
+    $(this).html(temp);
+  })
 });
